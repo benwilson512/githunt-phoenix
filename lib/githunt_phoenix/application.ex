f1015d0ef434ef1ca6568cb4f6a60af7de477177
Ben Wilson <benwilson512@gmail.com>
2017-06-14 21:05:55 -0400

    go back to local manager

--- lib/githunt_phoenix/application.ex
+++ lib/githunt_phoenix/application.ex
@@ -12,6 +12,7 @@ defmodule GitHunt.Application do
       supervisor(GitHunt.Repo, []),
       # Start the endpoint when the application starts
       supervisor(GitHunt.Web.Endpoint, []),
+      supervisor(Absinthe.Subscriptions.Manager, [GitHunt.Web.Endpoint]),
       # Start your own worker by calling: GitHunt.Worker.start_link(arg1, arg2, arg3)
       # worker(GitHunt.Worker, [arg1, arg2, arg3]),
     ]
